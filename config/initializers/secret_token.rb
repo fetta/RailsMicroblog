64f5417401e408e205a52f8ca251346549dbff2b
Jony <jony@mailinator.com>
2013-12-31 15:51:34 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Microblog::Application.config.secret_key_base = '4ade27a23d05f2a3b48524921686ef879da5f4b56bed808695acd5c6c8747111a490e11362f0ca5d3c5db67371ca75fad26d850d46da26174b4ddf9d93c430d5'
