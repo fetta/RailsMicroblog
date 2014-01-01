68de62310d8fccb66ff8668de6ede4025ac816d7
Jony <jony@mailinator.com>
2014-01-02 00:57:59 +0100

    layout, tytuly, widoki

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  # capybara - don't yet know why :)
+  config.include Capybara::DSL
 end
