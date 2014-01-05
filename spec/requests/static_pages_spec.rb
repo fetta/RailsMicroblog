425ef4037dbe4fc8695defad156d626bb0621139
Jony <jony@mailinator.com>
2014-01-05 15:24:03 +0100

    layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,36 @@
 require 'spec_helper'
 
 describe "Static pages" do
+subject { page }
 
   describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Ruby on Rails Tutorial Microblog'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Microblog")
-    end
+  before { visit root_path }
+  
+    it { should have_content('Microblog') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
     
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Microblog | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
 
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Microblog | About Us")
-    end
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
   
-  describe "Contact page" do
-    
-    it "should have the title 'Ruby on Rails Tutorial Microblog | Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title('Ruby on Rails Tutorial Microblog | Contact')
-    end
-    
-    it "should have the content 'contact page'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('contact page')
-    end
-  end
 end
