6e252f7565c24d40134d529f081b8012ded0fe1e
Jony <jony@mailinator.com>
2014-01-11 15:13:15 +0100

    finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,37 @@
 class UsersController < ApplicationController
+  def index
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to my Microblogging platform!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, 
+                      :password, :password_confirmation)
   end
 
 end
